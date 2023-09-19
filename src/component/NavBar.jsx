9958859740da427b0d73d3352a61dd9ee6ec0f01
yaho <yaser.hoseini86@gmail.com>
2023-09-19 15:04:16 +0330

    add border to navbar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -2,7 +2,7 @@ import DarkTheme from "./DarkTheme";
 
 function NavBar() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 border-b-2">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
